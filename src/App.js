74bc01fd14617cd6cbb3c855373fcf3f0a3ecedf
Carlos Abril <carlos.abril87@outlook.com>
2021-07-10 15:51:32 -0400

    object 3d controls

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-// import Model from './components/Three/Scene';
-import Player from "./Player"
-import ModelHut from './Hut';
-import React, { Suspense } from 'react';
-import { Physics } from "@react-three/cannon"
-import { Canvas, } from '@react-three/fiber';
-import Lights from './components/Three/Lights';
-import { OrbitControls, Stars } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import './App.css';
+
+import { OrbitControls, Stars } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Suspense } from 'react';
+import Model from './Scene';
+
+
+
+
 
 function App() {
   return (
     <>
-
-      <Canvas
-        colorManagement
-        shadowMap
-        shadows
-        
-        
-      camera={{position: [0,0,4454], near: 1 , far: 51500}}
-
-      >
-        
-        <Stars />
+      <Canvas camera={{ position: [0, 50, 50], near: 1, far: 100000 }}>
+        <gridHelper args={[100, 100, `white`, `gray`]} divisions={10} />
         <OrbitControls />
-        <Lights />
-          {/* <Model /> */}
-          <Physics>
-        <Suspense fallback={null}>
+        <ambientLight />
+        <Stars radius={5000} count={50900} factor={4} />
+        <Physics>
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+        </Physics>
 
-          <Player />
-          <ModelHut />
-        </Suspense>
-          </Physics>
 
 
       </Canvas>
+
     </>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,65 +1,29 @@
-import * as THREE from "three"
-import { useEffect, useRef, useState } from "react"
-import { useSphere } from "@react-three/cannon"
-import { useThree, useFrame } from "@react-three/fiber"
-import Model from "./components/Three/Scene"
+import { useEffect, useState } from "react"
 
-const SPEED = 5
+
+
 const keys = { KeyW: "forward", KeyS: "backward", KeyA: "left", KeyD: "right", Space: "jump" }
 const moveFieldByKey = (key) => keys[key]
-const direction = new THREE.Vector3()
-const frontVector = new THREE.Vector3()
-const sideVector = new THREE.Vector3()
-const rotation = new THREE.Vector3()
-const speed = new THREE.Vector3()
+
 
 const usePlayerControls = () => {
   const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false })
   useEffect(() => {
     const handleKeyDown = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
     const handleKeyUp = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
+    // const handleKeyRight = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
+    // const handleKeyLeft = (e) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
     document.addEventListener("keydown", handleKeyDown)
     document.addEventListener("keyup", handleKeyUp)
+    // document.addEventListener("keyright", handleKeyRight)
+    // document.addEventListener("keyleft", handleKeyLeft)
     return () => {
       document.removeEventListener("keydown", handleKeyDown)
       document.removeEventListener("keyup", handleKeyUp)
+      // document.removeEventListener("keyright", handleKeyRight)
+      // document.removeEventListener("keyleft", handleKeyLeft)
     }
   }, [])
   return movement
 }
-
-const Player = (props) => {
-    const ship = useRef()
-    const [ref, api] = useSphere(() => ({ mass: 0, type: "Dynamic", position: [0, 0, 10000], ...props }))
-    const { forward, backward, left, right, jump } = usePlayerControls()
-    const { camera } = useThree()
-    const velocity = useRef([0, 0, 0])
-    useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [])
-    useFrame((state) => {
-    //   ref.current.getWorldPosition(camera.position)
-      frontVector.set(0, 0, Number(backward) - Number(forward))
-      sideVector.set(Number(left) - Number(right), 0, 0)
-      direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(camera.rotation)
-      speed.fromArray(velocity.current)
-      ship.current.children[0].rotation.x = THREE.MathUtils.lerp(
-        ship.current.children[0].rotation.x,
-        Math.sin((speed.length() > 1) * state.clock.elapsedTime * 10) / 6,
-        0.1,
-      )
-      ship.current.rotation.copy(camera.rotation)
-    //   ship.current.position.copy(camera).add(camera.getWorldDirection(rotation).multiplyScalar(1))
-      api.velocity.set(direction.x, velocity.current[1], direction.z)
-      if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) api.velocity.set(velocity.current[0], 10, velocity.current[2])
-    })
-    return (
-      <>
-        <mesh ref={ref} />
-        <group ref={ship} onPointerMissed={(e) => (ship.current.children[0].rotation.x)}>
-          <Model  />
-        </group>
-      </>
-    )
-  }
-
-  export default Player
-  
+export default usePlayerControls
